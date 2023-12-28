adad6ed7cdd8ad71a61915c3b70d540c2bd0fb7d
Dibya <selfstudyhack@gmail.com>
2023-12-28 21:25:42 +0530

    DIBYA: hamburger menu added(stylings included), and other stylings updated

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,23 +1,22 @@
 import React from "react";
-import {  NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "./../../public/images/odiben.jpg";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
   const { user } = useSelector((state) => state.users);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   return (
     <header className="header_container flex justify-around items-center w-[100vw] bg-white p-2 shadow-md sticky top-0 left-0 z-50">
-      <div className="logo w-1/6 flex justify-center">
+      <Link to="/" className="logo w-1/6 flex justify-center">
         <img src={Logo} height="32px" width="32px" alt="logo" />
-      </div>
+      </Link>
       <nav className="menus w-3/6 hidden md:block">
         <div className="space-x-8">
           <NavLink to="/" className="py-[0.5rem]">
@@ -29,19 +28,21 @@ function Header() {
           <NavLink to="/about" className="py-[0.5rem]">
             About Us
           </NavLink>
-          {(!user.isAdmin)&&<NavLink to="/my-items" className="py-[0.5rem]">
-            My Items
-          </NavLink>}
-          {user.isAdmin&&
-           <NavLink to="/admin-dashboard" className="py-[0.5rem]">
-           Admin Dashboard
-         </NavLink>}
+          {!user.isAdmin && (
+            <NavLink to="/my-items" className="py-[0.5rem]">
+              My Items
+            </NavLink>
+          )}
+          {user.isAdmin && (
+            <NavLink to="/admin-dashboard" className="py-[0.5rem]">
+              Admin Dashboard
+            </NavLink>
+          )}
         </div>
       </nav>
       <div className="icons flex justify-evenly w-2/6 text-purple-800 text-2xl">
         <div className="wallet">
-          {(!user.isAdmin)?
-          <AccountBalanceWalletIcon/>:""}
+          {!user.isAdmin ? <AccountBalanceWalletIcon /> : ""}
         </div>
         <div className="wishlist">
           <FavoriteBorderIcon />
@@ -50,15 +51,21 @@ function Header() {
           <ShoppingCartIcon />
         </div>
         <div className="profile">
-          <AccountCircleIcon onClick={()=>{
-            (!user.isAdmin)?navigate("/my-profile"):navigate("/admin-profile");
-          }} />
+          <AccountCircleIcon
+            onClick={() => {
+              !user.isAdmin
+                ? navigate("/my-profile")
+                : navigate("/admin-profile");
+            }}
+          />
         </div>
         <div className="logout-lined">
-          <LogoutOutlinedIcon onClick={()=>{
-            localStorage.removeItem("token");
-            navigate("/login");
-          }}/>
+          <LogoutOutlinedIcon
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/login");
+            }}
+          />
         </div>
       </div>
     </header>
--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Filter from "./Filter";
 import ViewPort from "./ViewPort";
 
 function Main() {
+  const [isActive, setIsActive] = useState(true);
+  const filterToggle = () => {
+    setIsActive((prev) => !prev);
+  };
   return (
     <main className="flex justify-between">
-      <div className="sidebar w-2/12  hidden md:block">
+      <div className="sidebar w-2/12 min-w-[200px] hidden md:block ">
         <Filter />
+      </div>
+      <div
+        onClick={filterToggle}
+        className={`${
+          isActive ? "" : "filter-active"
+        } hamburger-icon sticky top-[4rem] left-4 w-[2.5rem] h-[2.5rem] flex flex-col justify-center items-center border-purple-800 border-2 rounded-md md:hidden`}
+      >
+        <div className="hamburger-line block w-8 h-1 my-[0.12rem] bg-purple-800"></div>
+        <div className=" hamburger-line block w-8 h-1 my-[0.12rem] bg-purple-800"></div>
+        <div className="hamburger-line block w-8 h-1 my-[0.12rem] bg-purple-800"></div>
       </div>
       <div className="viewport w-10/12">
         <ViewPort />
